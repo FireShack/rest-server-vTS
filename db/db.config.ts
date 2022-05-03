849ff6783472e93dbc448584d4e674eb7616dcb7
Franco <francoguardini10@gmail.com>
2022-05-03 09:59:44 -0300

    db connection added

--- db/db.config.ts
+++ db/db.config.ts
@@ -1,9 +1,20 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("users", "root", "", {
+// const db_name = process.env.DB_NAME;
+// const db_username = process.env.DB_USERNAME;
+// const db_password = process.env.DB_PASSWORD;
+// const db_host = process.env.DB_PASSWORD;
+
+const sequelize = new Sequelize("fires-server-ts", "root", "", {
   host: "localhost",
-  dialect: "mysql",
-  //   logging: false,
+  dialect: "mariadb",
 });
 
-export default db;
+export const initDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("DB online");
+  } catch (error) {
+      console.log("There was an error", error)
+  }
+};
--- server.ts
+++ server.ts
@@ -2,11 +2,15 @@ import express, { json } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { users } from "./routes/users.route";
+import { initDB } from "./db/db.config";
 
 // Basic configs
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+// DB connection
+initDB();
 
 // Middlewares
 app.use(cors());
